ea865204de8601b131ecfa431ceb6b77e6f4ada6
uwagboe <sm.uwagboe@gmail.com>
2020-02-11 11:16:32 -0500

    the collection view cells are now loading

--- Unit4Assessment/Unit4Assessment/Controllers/LookAtMyCardsController.swift
+++ Unit4Assessment/Unit4Assessment/Controllers/LookAtMyCardsController.swift
@@ -17,18 +17,49 @@ class LookAtMyCardsController: UIViewController {
     
     public var dataP : DataPersistence<String>!
     
-    private var instanceOfCardsView = LookAtMyCardsView()
+    private var cardsView = LookAtMyCardsView()
 
     override func loadView() {
-        view = instanceOfCardsView
+        view = cardsView
     }
+    
+    //need the model to populate things inside of my collection view...
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .blue
+        cardsView.collectionV.dataSource = self
+        cardsView.collectionV.delegate = self
         
+        cardsView.searchBar.delegate = self
+
+        // need to set the cell here.
+        
+        cardsView.collectionV.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cardsCell")
+        
+        view.backgroundColor = .systemGroupedBackground
     }
     
 
+}
+
+extension LookAtMyCardsController: UISearchBarDelegate {
+    
+}
+
+extension LookAtMyCardsController: UICollectionViewDelegateFlowLayout {
+    
+}
+
+extension LookAtMyCardsController: UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cardsCell", for: indexPath)
+        
+        cell.backgroundColor = .white
+        
+        return cell
+    }
 }
