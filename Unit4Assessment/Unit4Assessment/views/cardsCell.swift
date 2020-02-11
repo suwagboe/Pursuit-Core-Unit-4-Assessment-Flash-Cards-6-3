f9bedf15eec286568afffd97fcb107c733af2b1f
uwagboe <sm.uwagboe@gmail.com>
2020-02-11 11:20:15 -0500

    the cells now appear from  the default number of cell that I put

---
+++ Unit4Assessment/Unit4Assessment/views/cardsCell.swift
@@ -0,0 +1,16 @@
+//
+//  cardsCell.swift
+//  Unit4Assessment
+//
+//  Created by Pursuit on 2/11/20.
+//  Copyright © 2020 Alex Paul. All rights reserved.
+//
+
+import UIKit
+import  ImageKit
+
+class cardsCell: UICollectionViewCell {
+
+    
+    
+}
